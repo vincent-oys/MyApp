c32f1fbe31f028f6449f881d75f00b9b8e58191a
vincent-oys <vincent.ong90@gmail.com>
2020-10-18 01:23:43 +0800

    Login Register completed

--- client/src/Components/Contents/Journal.js
+++ client/src/Components/Contents/Journal.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 class Journal extends React.Component {
-    render() {
-        return (
-            <div>
-                Journal Component
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h3>Journal Component</h3>
+        <p>{this.props.loggedInStatus}</p>
+      </div>
+    );
+  }
 }
 
-export default Journal
+export default Journal;
