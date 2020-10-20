4d862f9070c33dcf7023bc2990d282178d7e09ea
vincent-oys <vincent.ong90@gmail.com>
2020-10-20 11:25:01 +0800

    heroku hosting

--- server.js
+++ server.js
@@ -10,13 +10,8 @@ const app = express();
 require("dotenv").config();
 
 // middleware
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use(cookieParser());
 
